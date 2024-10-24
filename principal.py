5717d413fc5972e6be0e62779bb7c9b0db1069b7
YanMN50 <yan.matos@ba.estudante.senai.br>
2024-10-24 17:28:05 +0000

    salvar

---
+++ principal.py
@@ -0,0 +1,13 @@
+import os
+os.system("clss || clear")
+
+# git branch
+# git checkout -b
+# fix = corrigir
+# feature = criar  
+
+print("Adicionando funcionalidade: função inss.")
+
+def inss():
+    # Considere feito.
+    pass
