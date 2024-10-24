980c100123c8ef1c5e633b2f754e0916c6e235a1
YanMN50 <yan.matos@ba.estudante.senai.br>
2024-10-24 17:55:44 +0000

    salvar

--- principal.py
+++ principal.py
@@ -2,10 +2,12 @@ import os
 os.system("clss || clear")
 
 # git branch
-# git checkout -b
+# git checkout -b = criar
+# git branch -d = delete
 # fix = corrigir
 # feature = criar  
 # git merge = juntar
+
 
 print("Adicionando funcionalidade: função inss.")
 
